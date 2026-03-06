d9aad694c4744c1d8e786d8724b72d3213d9b205
MatteoCiardi <ciardimatteo99@gmail.com>
2026-03-06 17:56:20 +0100

    completata logica comparatore

--- src/components/comparator/Comparator.jsx
+++ src/components/comparator/Comparator.jsx
@@ -1,7 +1,141 @@
+import { useState } from 'react';
+
 import './comparator.css'
 
-export default function Comparator() {
+export default function Comparator({ games, availableGames }) {
+    if (games.length === 0) return null;
+
+    const [showList, setShowList] = useState(false);
+    const [rightGame, setRightGame] = useState(null);
+
+    const handleSelectGame = async (selectedId) => {
+        try {
+            const res = await fetch(`http://localhost:3001/games/${selectedId}`);
+            const data = await res.json();
+            setRightGame(data.game);
+            setShowList(false);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    const compareValues = (leftValue, rightValue, type) => {
+        if (type === 'higherBetter')
+            return leftValue > rightValue ? 'left' : leftValue < rightValue ? 'right' : 'equal';
+
+        if (type === 'lowerBetter')
+            return leftValue < rightValue ? 'left' : leftValue > rightValue ? 'right' : 'equal';
+
+        return 'equal';
+    }
+
+    const priceResult = rightGame
+        ? compareValues(games[0].price, rightGame.price, 'lowerBetter')
+        : null;
+
+    const ratingResult = rightGame
+        ? compareValues(games[0].rating, rightGame.rating, 'higherBetter')
+        : null;
+
     return (
-        <h1>COMPARATORE</h1>
+        <div className="comparator-container">
+
+            <div className="comparator-left">
+                <h3>{games[0].title}</h3>
+                <img src={games[0].image} alt={games[0].title} />
+
+                <p>{games[0].category}</p>
+
+                <p
+                    style={{
+                        color:
+                            priceResult === 'left'
+                                ? 'green'
+                                : priceResult === 'right'
+                                    ? 'red'
+                                    : 'black'
+                    }}
+                >
+                    Prezzo: €{games[0].price}
+                </p>
+
+                <p
+                    style={{
+                        color:
+                            ratingResult === 'left'
+                                ? 'green'
+                                : ratingResult === 'right'
+                                    ? 'red'
+                                    : 'black'
+                    }}
+                >
+                    Rating: {games[0].rating}
+                </p>
+            </div>
+
+            <div className="comparator-right">
+                {rightGame ? (
+                    <>
+                        <h3>{rightGame.title}</h3>
+                        <img src={rightGame.image} alt={rightGame.title} />
+
+                        <p>{rightGame.category}</p>
+
+                        <p
+                            style={{
+                                color:
+                                    priceResult === 'right'
+                                        ? 'green'
+                                        : priceResult === 'left'
+                                            ? 'red'
+                                            : 'black'
+                            }}
+                        >
+                            Prezzo: €{rightGame.price}
+                        </p>
+
+                        <p
+                            style={{
+                                color:
+                                    ratingResult === 'right'
+                                        ? 'green'
+                                        : ratingResult === 'left'
+                                            ? 'red'
+                                            : 'black'
+                            }}
+                        >
+                            Rating: {rightGame.rating}
+                        </p>    
+                    </>
+                ) : (
+                    <>
+                        <button
+                            className="add-game-btn"
+                            onClick={() => setShowList(!showList)}
+                        >
+                            +
+                        </button>
+
+                        {showList && (
+                            <div className="game-selection-list">
+                                {availableGames
+                                    .filter(g => g.id !== games[0].id)
+                                    .map(g => (
+                                        <div
+                                            key={g.id}
+                                            className="game-option"
+                                            onClick={() => handleSelectGame(g.id)}
+                                        >
+                                            {g.title}
+                                        </div>
+                                    ))}
+                            </div>
+                        )}
+                    </>
+                )}
+            </div>
+
+
+        </div>
     )
 }
--- src/pages/gamedetail/GameDetail.jsx
+++ src/pages/gamedetail/GameDetail.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom'
+import { GlobalContext } from '../../context/GlobalContext'
 
 import Comparator from '../../components/comparator/Comparator';
 
@@ -7,7 +8,9 @@ import './gamedetail.css'
 
 export default function GameDetail() {
     const { id } = useParams();
+    const { games } = useContext(GlobalContext)
     const [game, setGame] = useState(null)
+    const [compareList, setCompareList] = useState([]);
 
     useEffect(() => {
         const fetchGame = async () => {
@@ -26,28 +29,41 @@ export default function GameDetail() {
 
     if (!game) return <p>Gioco non trovato </p>
 
+    const addToCompare = (game) => {
+        if (!compareList.some(g => g.id === game.id)) {
+            setCompareList([...compareList, game]);
+        }
+    }
+
     return (
-        <div className="gamedetail-container">
-            <div className="gamedetail-left">
-                <img src={game.image} alt={game.title} />
-                <div className="description">{game.description}</div>
-            </div>
+        <>
+            <div className="gamedetail-container">
+                <div className="gamedetail-left">
+                    <img src={game.image} alt={game.title} />
+                    <div className="description">{game.description}</div>
+                </div>
 
-            <div className="gamedetail-right">
-                <h2>Informazioni</h2>
-                <p><b>Categoria:</b> {game.category}</p>
-                <p><b>Prezzo:</b> €{game.price}</p>
-                <p><b>Rating:</b> {game.rating}</p>
-                <p><b>Anno di uscita:</b> {game.releaseYear}</p>
-                <p><b>Developer:</b> {game.developer}</p>
-                <p><b>Publisher:</b> {game.publisher}</p>
-                {game.translated && <p><b>Tradotto:</b> Sì</p>}
+                <div className="gamedetail-right">
+                    <h2>Informazioni</h2>
+                    <p><b>Categoria:</b> {game.category}</p>
+                    <p><b>Prezzo:</b> €{game.price}</p>
+                    <p><b>Rating:</b> {game.rating}</p>
+                    <p><b>Anno di uscita:</b> {game.releaseYear}</p>
+                    <p><b>Developer:</b> {game.developer}</p>
+                    <p><b>Publisher:</b> {game.publisher}</p>
+                    {game.translated && <p><b>Tradotto:</b> Sì</p>}
 
-                <button>
-                    COMPARA
-                </button>
+                    <button
+                        onClick={() => addToCompare(game)}
+                    >
+                        COMPARA
+                    </button>
+                </div>
             </div>
-            <Comparator />
-        </div>
+            <Comparator
+                games={compareList}
+                availableGames={games}
+            />
+        </>
     )
 }
