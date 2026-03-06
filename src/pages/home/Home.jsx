3804fad4aa6859f086a8f87b6d25339f3e5085f0
MatteoCiardi <ciardimatteo99@gmail.com>
2026-03-06 01:01:32 +0100

    aggiunta logica filtri per categoria

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState, useMemo } from 'react'
 import { GlobalContext } from '../../context/GlobalContext'
+import { RxReset } from 'react-icons/rx'
 
 import GameCard from '../../components/gamecard/GameCard'
 
@@ -8,77 +9,84 @@ import './home.css'
 export default function Home() {
     const { games } = useContext(GlobalContext)
     const [query, setQuery] = useState('')
+    const [categoryFilter, setCategoryFilter] = useState('all');
 
     const handleSearch = (value) => {
         setQuery(value)
     }
 
+    const handleCategory = (category) => {
+        setCategoryFilter(category)
+    }
+
     const filteredGames = useMemo(() => {
-        return games.filter(game => 
-            game.title.toLowerCase().includes(query.toLowerCase())
-        )
-    }, [games, query])
+        return games.filter(game => {
+            const matchTitle = game.title
+                .toLowerCase()
+                .includes(query.toLowerCase())
+
+            const matchCategory =
+                categoryFilter === 'all' || game.category === categoryFilter
+
+            return matchTitle && matchCategory
+        })
+    }, [games, query, categoryFilter])
 
     return (
         <>
             <div className='home-container'>
                 <section className='filters'>
                     <input
+                        className='searchbar'
                         type='text'
                         placeholder='Cerca..'
                         value={query}
                         onChange={(e) => handleSearch(e.target.value)}
                     />
-                    <div className='checkboxes'>
-                        <label htmlFor='category' className='cb-label'>
-                            <input
-                                type='checkbox'
-                                id='category'
-                            />
+                    <div className='cat-buttons'>
+                        <div className="cat-header">
+                            <span className='cat-title'>Categorie</span>
+                            <button
+                                onClick={() => handleCategory('all')}
+                                className='reset-button'>
+                                <RxReset />
+                            </button>
+                        </div>
+                        <button
+                            onClick={() => handleCategory('RPG')}
+                            className='cat-button'>
                             RPG
-                        </label>
-                        <label htmlFor='category' className='cb-label'>
-                            <input
-                                type='checkbox'
-                                id='category'
-                            />
+                        </button>
+                        <button
+                            onClick={() => handleCategory('Action RPG')}
+                            className='cat-button'>
                             Action RPG
-                        </label>
-                        <label htmlFor='category' className='cb-label'>
-                            <input
-                                type='checkbox'
-                                id='category'
-                            />
+                        </button>
+                        <button
+                            onClick={() => handleCategory('Metroidvania')}
+                            className='cat-button'>
                             Metroidvania
-                        </label>
-                        <label htmlFor='category' className='cb-label'>
-                            <input
-                                type='checkbox'
-                                id='category'
-                            />
+                        </button>
+                        <button
+                            onClick={() => handleCategory('Management Simulation')}
+                            className='cat-button'>
                             Management Simulation
-                        </label>
-                        <label htmlFor='category' className='cb-label'>
-                            <input
-                                type='checkbox'
-                                id='category'
-                            />
+                        </button>
+                        <button
+                            onClick={() => handleCategory('Farming Simulation')}
+                            className='cat-button'>
                             Farming Simulation
-                        </label>
-                        <label htmlFor='category' className='cb-label'>
-                            <input
-                                type='checkbox'
-                                id='category'
-                            />
+                        </button>
+                        <button
+                            onClick={() => handleCategory('Roguelike')}
+                            className='cat-button'>
                             Roguelike
-                        </label>
-                        <label htmlFor='category' className='cb-label'>
-                            <input
-                                type='checkbox'
-                                id='category'
-                            />
+                        </button>
+                        <button
+                            onClick={() => handleCategory('CRPG')}
+                            className='cat-button'>
                             CRPG
-                        </label>
+                        </button>
                     </div>
                 </section>
                 <section>
